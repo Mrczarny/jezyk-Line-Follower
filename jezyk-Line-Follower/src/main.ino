133df7118d8eb164c8cb7d23d9f5a0fa3a53015f
mrczarny <j.adamski@zsebydgoszcz.onmicrosoft.com>
2024-11-18 15:21:40 +0100

    added movement lib

--- jezyk-Line-Follower/src/main.ino
+++ jezyk-Line-Follower/src/main.ino
@@ -1,70 +1,55 @@
 #include <Arduino.h>
+#include <motors.h>
 
 #define A1_PIN 9
 #define A2_PIN 6
 #define B1_PIN 5
 #define B2_PIN 3
 
+Motors motors(A2_PIN,A1_PIN, B1_PIN, B2_PIN);
+
 void setup() {
   // put your setup code here, to run once:
+  motors.setup(
+    240,
+    255,
+    237,
+    251
+  );
   Serial.begin(9600);
-  pinMode(A1_PIN, OUTPUT);
-  pinMode(A2_PIN, OUTPUT);
-  pinMode(B1_PIN, OUTPUT);
-  pinMode(B2_PIN, OUTPUT);
-
 }
 
 void loop() {
-  //turnRight();
-  // stop();
-  // delay(5000);
-   turnLeft();
-  // stop();
-  // delay(5000);
-}
+  // put your main code here, to run repeatedly:
+  motors.forward();
+  delay(4000);
+  motors.stop();
+  delay(1000);
 
-void forward() {
-  analogWrite(A1_PIN, 255);
-  analogWrite(B2_PIN, 255);
+  motors.backward();
+  delay(4000);
+  motors.stop();
+  delay(1000);
+
+  motors.zeroLeft();
+  delay(2000);
+  motors.stop();
+  delay(1000);
+
+  motors.zeroRight();
+  delay(2000);
+  motors.stop();
+  delay(1000);
+
+    motors.left();
+  delay(2000);
+  motors.stop();
+  delay(1000);
+
+  motors.right();
+  delay(2000);
+  motors.stop();
   delay(1000);
 }
 
-void backward() {
-  analogWrite(A2_PIN, 255);
-  analogWrite(B1_PIN, 255);
-  delay(1000);
-}
-
-void stop() {
-  analogWrite(A1_PIN, 0);
-  analogWrite(A2_PIN, 0);
-  analogWrite(B1_PIN, 0);
-  analogWrite(B2_PIN, 0);
-  delay(1000);
-}
-
-void zeroTurnRight() {
-  analogWrite(A2_PIN, 255);
-  analogWrite(B2_PIN, 255);
-  delay(1000);
-}
-
-void zeroTurnLeft() {
-  analogWrite(A1_PIN, 255);
-  analogWrite(B1_PIN, 255);
-  delay(1000);
-}
-
-void turnRight() {
-  analogWrite(A2_PIN, 255);
-  analogWrite(B1_PIN, 180);
-  delay(1000);
-}
-
-void turnLeft() {
-  analogWrite(A2_PIN, 180);
-  analogWrite(B1_PIN, 255);
-  delay(1000);
-}
 

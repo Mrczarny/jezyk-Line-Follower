736f061aee2d51d968ee5a04abe2ce4d89762a73
Jan Adamski <jan.adamski@student.nhlstenden.com>
2024-12-09 15:10:17 +0100

    SCRUM-5 line follower actually follows the line

--- jezyk-Line-Follower/src/main.ino
+++ jezyk-Line-Follower/src/main.ino
@@ -1,57 +1,117 @@
 #include <Arduino.h>
 #include <motors.h>
+#include <line.h>
+#include <sonar.h>
+#include <rotation.h>
+#include <string.h>
 
-#define A1_PIN 9
-#define A2_PIN 6
-#define B1_PIN 5
-#define B2_PIN 3
+#define A1_PIN 6
+#define A2_PIN 7
+#define B1_PIN 4
+#define B2_PIN 5
 
-//test
+int linePins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+Motors motors(A1_PIN, A2_PIN, B2_PIN, B1_PIN);
+Line line(8, linePins);
+Sonar sonar(9, 10);
+Rotation rotation(2, 3);
 
-Motors motors(A2_PIN,A1_PIN, B1_PIN, B2_PIN);
+static int lastTime = 0;
 
-void setup() {
+static lineSensorData lineState;
+
+void setup()
+{
   // put your setup code here, to run once:
   motors.setup(
-    240,
-    255,
-    237,
-    251
-  );
+      0, 0, 255, 255);
   Serial.begin(9600);
+  line.setup(700);
+  lineState = line.readLine();
+  rotation.setup(&motors, 0, 0);
+  // Serial.println("setup");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  motors.forward();
-  delay(4000);
-  motors.stop();
-  delay(1000);
-
-  motors.backward();
-  delay(4000);
-  motors.stop();
-  delay(1000);
-
-  motors.zeroLeft();
-  delay(2000);
-  motors.stop();
-  delay(1000);
-
-  motors.zeroRight();
-  delay(2000);
-  motors.stop();
-  delay(1000);
-
-    motors.left();
-  delay(2000);
-  motors.stop();
-  delay(1000);
-
-  motors.right();
-  delay(2000);
-  motors.stop();
-  delay(1000);
+  lineState = line.readLine();
+  uint8_t decimal = convertToBinary(lineState);
+    switch (decimal)
+    {
+    default:
+      break;
+    
+    case 0b00011000:
+      motors.forward(255);
+      break;
+    case 0b11110000:
+    case 0b01110000:
+    case 0b00110000:
+    case 0b01100000:
+    case 0b11000000:
+      motors.zeroRight(255);
+      break;
+    case 0b00001100:
+    case 0b00001110:
+    case 0b00001111:
+    case 0b00000110:
+    case 0b00000011:
+      motors.zeroLeft(255);
+      break;
+    case 0b11111111:
+      rotation.moveForwardFor(5);
+      if (convertToBinary(line.readLine()) == 0b11111111)
+      {
+        motors.stop();
+        while(1);
+      }
+      
+      break;
+    }
+  
 }
 
+int detectChange()
+{
+  // line
+  lineSensorData newLineState = line.readLine();
+  for (int i = 0; i < 8; i++)
+  {
+    if (newLineState.linePoints[i].isLine != lineState.linePoints[i].isLine)
+    {
+      lineState = newLineState;
+      return 1;
+    }
+  }
 
+  // object
+  // use milis
+  // int currentTime = millis();
+  // if (currentTime - lastTime > 1000)
+  // {
+  //   int d = sonar.getDistance();
+  //   // Serial.println(d);
+  //   lastTime = currentTime;
+  //   if (d < 10)
+  //   {
+  //     return 2;
+  //   }
+  // }
+
+  return 0;
+}
+
+uint8_t convertToBinary(lineSensorData line)
+{
+  uint8_t decimal = 00000000;
+  for (int i = 0; i < 8; i++)
+  {
+    if (line.linePoints[i].isLine)
+    {
+      decimal |= 1 << i;
+    }
+  }
+  //Serial.println(decimal);
+  return decimal;
+}
